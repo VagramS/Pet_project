2fd2d7f305d20cb86ae76e4e66a5e87924debf47
Vagram <Vagramsaakyan@gmail.com>
2024-04-28 14:46:08 +0200

    Postgres connection

--- src/Controllers/storeController.js
+++ src/Controllers/storeController.js
@@ -1,20 +1,27 @@
+const storeService = require("../Services/storeService");
+
 const getAllItems = (req, res) => {
+    const allItems = storeService.getAllItems();
     res.send("Get all Items");
 };
 
 const getOneItem = (req, res) => {
+    const item = storeService.getOneItem();
     res.send("Get a certain item");
 };
 
 const createNewComputer = (req, res) => {
+    const newItem = storeService.createNewComputer();
     res.send("Create a new computer");
 };
 
 const updateAnItem = (req, res) => {
+    const updatedItem = storeService.updateAnItem();
     res.send("Update an existing computer");
 };
 
 const deleteOneComputer = (req, res) => {
+    const deletedItem = storeService.deleteOneComputer();
     res.send("Delete a computer");
 };
 
---
+++ src/Services/storeService.js
@@ -0,0 +1,27 @@
+const getAllItems = () => {
+    return;
+};
+
+const getOneItem = () => {
+    return;
+};
+
+const createNewComputer = () => {
+    return;
+};
+
+const updateAnItem = () => {
+    return;
+};
+
+const deleteOneComputer = () => {
+    return;
+};
+
+module.exports = {
+    getAllItems,
+    getOneItem,
+    createNewComputer,
+    updateAnItem,
+    deleteOneComputer,
+};
